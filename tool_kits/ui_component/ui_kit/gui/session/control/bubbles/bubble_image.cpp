8a2e222adfd456e07c4cdc1c334d3ed5233e46a1
ChenYang <idoop@msn.cn>
2023-08-14 00:03:45 +0800

    copy from the Ethan's second time modify

--- tool_kits/ui_component/ui_kit/gui/session/control/bubbles/bubble_image.cpp
+++ tool_kits/ui_component/ui_kit/gui/session/control/bubbles/bubble_image.cpp
@@ -224,7 +224,7 @@ bool MsgBubbleImage::OnMenu( ui::EventArgs* arg )
 				show_retweet = false;
 		}
 	}
-	PopupMenu(false, true, show_retweet);
+	PopupMenu(true, true, show_retweet);
 	return false;
 }
 
@@ -242,4 +242,88 @@ bool MsgBubbleImage::NeedDownloadResourceEx()
 	else
 		return true;
 }
+
+void MsgBubbleImage::OnMenuCopy() {
+    HBITMAP hBitmap = NULL;
+    //背景色设置为白色。
+    Gdiplus::Color backColor = Gdiplus::Color(255, 255, 255, 255);
+    // Status sts
+    if (PathFileExists(path_.c_str())) {
+        Gdiplus::Bitmap* pBitmap = Gdiplus::Bitmap::FromFile(path_.c_str());
+        Gdiplus::Status sts = pBitmap->GetLastStatus();
+        sts = pBitmap->GetHBITMAP(backColor, &hBitmap);
+        if (sts == Gdiplus::Ok) {
+            int k = 0;
+        }
+    }
+    int error = GetLastError();
+
+    if (!::OpenClipboard(NULL))
+        return;
+    ::EmptyClipboard();
+    BITMAP bm;
+    ::GetObject(hBitmap, sizeof(bm), &bm);
+
+    BITMAPINFOHEADER bi;
+    ::ZeroMemory(&bi, sizeof(BITMAPINFOHEADER));
+    bi.biSize = sizeof(BITMAPINFOHEADER);
+    bi.biWidth = bm.bmWidth;
+    bi.biHeight = bm.bmHeight;
+    bi.biPlanes = 1;
+    bi.biBitCount = bm.bmBitsPixel;
+    bi.biCompression = BI_RGB;
+    if (bi.biBitCount <= 1)  // make sure bits per pixel is valid
+        bi.biBitCount = 1;
+    else if (bi.biBitCount <= 4)
+        bi.biBitCount = 4;
+    else if (bi.biBitCount <= 8)
+        bi.biBitCount = 8;
+    else  // if greater than 8-bit, force to 24-bit
+        bi.biBitCount = 24;
+
+    // Get size of color table.
+    SIZE_T dwColTableLen = (bi.biBitCount <= 8) ? (1 << bi.biBitCount) * sizeof(RGBQUAD) : 0;
+
+    // Create a device context with palette
+    HDC hDC = ::GetDC(NULL);
+    HPALETTE hPal = static_cast<HPALETTE>(::GetStockObject(DEFAULT_PALETTE));
+    HPALETTE hOldPal = ::SelectPalette(hDC, hPal, FALSE);
+    ::RealizePalette(hDC);
+
+    // Use GetDIBits to calculate the image size.
+    ::GetDIBits(hDC, hBitmap, 0, static_cast<UINT>(bi.biHeight), NULL, reinterpret_cast<LPBITMAPINFO>(&bi), DIB_RGB_COLORS);
+    // If the driver did not fill in the biSizeImage field, then compute it.
+    // Each scan line of the image is aligned on a DWORD (32bit) boundary.
+    if (0 == bi.biSizeImage)
+        bi.biSizeImage = ((((bi.biWidth * bi.biBitCount) + 31) & ~31) / 8) * bi.biHeight;
+
+    // Allocate memory
+    HGLOBAL hDIB = ::GlobalAlloc(GMEM_MOVEABLE, sizeof(BITMAPINFOHEADER) + dwColTableLen + bi.biSizeImage);
+    if (hDIB) {
+        union tagHdr_u {
+            LPVOID p;
+            LPBYTE pByte;
+            LPBITMAPINFOHEADER pHdr;
+            LPBITMAPINFO pInfo;
+        } Hdr;
+
+        Hdr.p = ::GlobalLock(hDIB);
+        // Copy the header
+        ::CopyMemory(Hdr.p, &bi, sizeof(BITMAPINFOHEADER));
+        // Convert/copy the image bits and create the color table
+        int nConv = ::GetDIBits(hDC, hBitmap, 0, static_cast<UINT>(bi.biHeight), Hdr.pByte + sizeof(BITMAPINFOHEADER) + dwColTableLen, Hdr.pInfo,
+                                DIB_RGB_COLORS);
+        ::GlobalUnlock(hDIB);
+        if (!nConv) {
+            ::GlobalFree(hDIB);
+            hDIB = NULL;
+        }
+    }
+    if (hDIB)
+        ::SetClipboardData(CF_DIB, hDIB);
+    ::CloseClipboard();
+    ::SelectPalette(hDC, hOldPal, FALSE);
+    ::ReleaseDC(NULL, hDC);
+}
+
 }
