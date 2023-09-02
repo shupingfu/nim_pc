451fff1d9e5e9f8492b0fb84f7eb4bdfdd8df380
ChenYang <idoop@msn.cn>
2023-09-02 16:21:11 +0800

    revert: 去除分页加载消息,还原逻辑

--- tool_kits/ui_component/ui_kit/callback/session/session_callback.cpp
+++ tool_kits/ui_component/ui_kit/callback/session/session_callback.cpp
@@ -188,33 +188,20 @@ void TalkCallback::OnQueryMsgCallback(nim::NIMResCode code, const std::string& q
 		<<query_id <<query_type <<code<< result.source_ << result.count_ << result.msglogs_.size();
 	
 	//QueryMsgAync 返回的消息为逆序排序 反向遍历加入vector
-    const int8_t once_load_cnt = 40; //单次加载消息条数
 	std::vector<nim::IMMessage> vec;
-    int8_t vec_curr = 0, vec_cnt = result.msglogs_.size() / once_load_cnt + 1;
-    auto vec_new = new std::vector<nim::IMMessage>[vec_cnt];
-	
-    int pushd_msg_cnt = 0;
+
 	for (auto iter = result.msglogs_.rbegin(); iter != result.msglogs_.rend(); iter++) {
-		if (pushd_msg_cnt == once_load_cnt)
-            vec_curr++;
-		vec_new[vec_curr].push_back(*iter);
-        std::this_thread::sleep_for(std::chrono::milliseconds(33)); //todo: 测试是否由网络并发下载图片导致拥堵延迟而崩溃
+        vec.push_back(*iter);
 	}
 
-	SessionBox* session_form = SessionManager::GetInstance()->FindSessionBox(query_id);
-    if (session_form) {
-        try {
-//            for (auto& v : *vec_new) {
-//                session_form->ShowMsgs(vec);
-//            }
-            for (vec_curr = 0; vec_curr < vec_cnt; vec_curr++) {
-                session_form->ShowMsgs(vec_new[vec_curr]);
-			}
-		} catch (std::exception& e) {
-            QLOG_ERR(L"session_form->ShowMsgs exception: {1}") << e.what();
-		}
-    }
-    delete []vec_new;
+    try {
+        SessionBox* session_form = SessionManager::GetInstance()->FindSessionBox(query_id);
+        if (session_form) {
+            session_form->ShowMsgs(vec);
+        }
+	} catch (std::exception& e) {
+        QLOG_ERR(L"session_form->ShowMsgs exception: {1}") << e.what();
+	}
 }
 
 void TalkCallback::OnQuerySessionListCallback(int unread_count, const nim::SessionDataList& session_list)
